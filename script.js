e4f9f74c70cd5734fbd2d0baac15ebcf4500d161
aziza <azizirage012@gmail.com>
2023-06-14 09:15:44 +0300

    added the form message to the google sheet

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var tablinkEL = document.getElementsByClassName("tab-links");
 var tabcontentEl = document.getElementsByClassName("tabcontents");
+const msg= document.getElementById("msg");
 
 function opentab(tabname) {
   for (tablink of tablinkEL) {
@@ -28,3 +29,24 @@ function closeme(){
   sidemenuEl.style.right= "-200px";
 
 }
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwuOBMlmW5Eln9WP0iZe2sGCab2_kXoVOfV18jCpoF_GI758pwGzBDkpbuplZfJVOXPoQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message sent successfully"
+        // messge will be removed after 5 seconds haddii kale waa uu iska yaalaaa
+        setTimeout(function(){
+          msg.innerHTML=""
+        }, 5000)
+
+        form.reset()
+
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
